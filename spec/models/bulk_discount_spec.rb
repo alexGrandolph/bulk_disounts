6a4e7cbec1337078e835fa61318f6c2145716449
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-04-21 17:08:43 -0600

    Feat: update validations in discount model to validate percentage is less than or equal to 100 and greater than 1

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -5,8 +5,6 @@ RSpec.describe BulkDiscount, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:percentage) }
     it { should validate_presence_of(:threshold) }
-    it { should validate_presence_of(:created_at) }
-    it { should validate_presence_of(:updated_at) }
   end
 
   describe 'relationships' do
