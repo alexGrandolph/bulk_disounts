b87db4acd181daf4d165acd41176dd337018e5e3
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-04-21 16:44:41 -0600

    Feat: add validations and relationship to the bulk discount model

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,6 +1,11 @@
 class BulkDiscount < ApplicationRecord
   
+  validates_presence_of :name 
+  validates_presence_of :percentage, comparison: { greater_than: 1, less_than: 100}
+  validates_presence_of :threshold, numbericality: true
+  validates_presence_of :created_at
+  validates_presence_of :updated_at
 
-
+  belongs_to :merchant
 
 end 
