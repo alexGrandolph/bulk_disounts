0217a1d8451df04e2dfaeaaf05d79ac41b60a293
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-04-21 17:43:28 -0600

    Feat: add link, route, action, and view for bulk_discounts/new

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -4,6 +4,17 @@ class BulkDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
    
   end
+
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = BulkDiscount.find(params[:id])
+  end
+
+  def new
+    
+  end
+  
+  
   
 
 end 
