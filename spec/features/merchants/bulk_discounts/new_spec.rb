f959650d266042620628ffb2dd9633f820c8ce52
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-04-22 10:12:02 -0600

    Test: add sad path test for entering discount percentage over 100 when creating a new discount

--- spec/features/merchants/bulk_discounts/new_spec.rb
+++ spec/features/merchants/bulk_discounts/new_spec.rb
@@ -21,5 +21,22 @@ RSpec.describe 'Merchant Bulk Discount New Page' do
       # save_and_open_page
       expect(page).to have_content("New 50 for 50")
     end 
+    #sad path
+    it 'Will not allow for a discount percentage to be greater than 100' do
+      merch4 = Merchant.create!(name: 'My Dog Skeeter', created_at: DateTime.now, updated_at: DateTime.now, status: 1)
+      disc1 = BulkDiscount.create!(name: '10 for 10%', percentage: 10, threshold: 10, merchant_id: merch4.id)
+      disc2 = BulkDiscount.create!(name: '5 for 5%', percentage: 5, threshold: 5, merchant_id: merch4.id)
+
+      visit "/merchants/#{merch4.id}/bulk_discounts"
+      
+      click_link "Create a New Bulk Discount"
+      fill_in "Name", with: "New 50 for 50"
+      fill_in "Percentage", with: 101
+      fill_in "Threshold", with: 5
+      click_on "Submit"
+      # save_and_open_page
+      expect(current_path).to eq("/merchants/#{merch4.id}/bulk_discounts/new")
+      expect(page).to have_content("It is company policy for discounts to be less than or eqaul to 100%")
+    end
   end
 end
