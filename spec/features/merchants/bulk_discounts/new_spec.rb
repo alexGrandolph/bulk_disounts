d928a8836b4fd8517ba4d69a11f7da79cd470047
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-04-26 15:35:18 -0600

    Test: add test for creating a new discount with a string for threshold

--- spec/features/merchants/bulk_discounts/new_spec.rb
+++ spec/features/merchants/bulk_discounts/new_spec.rb
@@ -56,6 +56,23 @@ RSpec.describe 'Merchant Bulk Discount New Page' do
       expect(page).to have_content("BAD! Discount Percentage Must Be Greater Than 0 AND Less Than 100")
     end
 
+    it 'Will not allow for threshold to be anything but an integer', :vcr do
+      merch4 = Merchant.create!(name: 'My Dog Skeeter', created_at: DateTime.now, updated_at: DateTime.now, status: 1)
+      disc1 = BulkDiscount.create!(name: '10 for 10%', percentage: 10, threshold: 10, merchant_id: merch4.id)
+      disc2 = BulkDiscount.create!(name: '5 for 5%', percentage: 5, threshold: 5, merchant_id: merch4.id)
+
+      visit "/merchants/#{merch4.id}/bulk_discounts"
+      
+      click_link "Create a New Bulk Discount"
+      fill_in "Name", with: "New 50 for 50"
+      fill_in "Percentage", with: 55
+      fill_in "Threshold", with: "cheese"
+      click_on "Submit"
+      # save_and_open_page
+      expect(current_path).to eq("/merchants/#{merch4.id}/bulk_discounts/new")
+      expect(page).to have_content("BAD! Discount Percentage Must Be Greater Than 0 AND Less Than 100")
+    end
+
 
   end
 end
