43f3199f086e3b8fb5dd2f71bf52dfc1eeb11d2d
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-04-26 15:33:51 -0600

    Feat: add private methods to disocunt controller to verify threshold and percentage as valid

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -46,17 +46,35 @@ class BulkDiscountsController < ApplicationController
   def update
     @merchant = Merchant.find(params[:merchant_id])
     @discount = BulkDiscount.find(params[:id])
-
-    if params[:percentage].to_i >= 100 || params[:percentage].to_i < 0
-      flash[:notice] = "BAD! Discount Percentage Must Be Greater Than 0 AND Less Than 100"
-      redirect_to "/merchants/#{@merchant.id}/bulk_discounts/#{@discount.id}/edit", method: :patch
+   
+    if invalid_percentage == true || invalid_threshold == true 
+      flash[:notice] = "BAD! Threshold and Percentage MUST be an Integer Between 0 and 100"
+      redirect_to "/merchants/#{@merchant.id}/bulk_discounts/#{@discount.id}/edit"
     else 
       @discount.update(name: params[:name], percentage: params[:percentage], threshold: params[:threshold])
       redirect_to "/merchants/#{@merchant.id}/bulk_discounts/#{@discount.id}"
     end 
   end
+
+  private
+      def invalid_percentage
+        params[:percentage].to_i >= 101 || params[:percentage].to_i < 0 
+      end 
+
+      def invalid_threshold
+        # binding.pry
+        params[:threshold].count("a-zA-Z") > 0
+      end
+
+      # def error_message
+      #   flash[:notice] = "BAD! Discount Percentage Must Be Greater Than 0 AND Less Than 100"
+      # end
+      
+      
   
   
   
 
 end 
+    # if params[:percentage].to_i >= 100 || params[:percentage].to_i < 0
+    #   flash[:notice] = "BAD! Discount Percentage Must Be Greater Than 0 AND Less Than 100"
--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -4,7 +4,7 @@ class BulkDiscount < ApplicationRecord
   validates_presence_of :percentage
   validates_numericality_of :percentage, less_than_or_equal_to: 100
   validates_numericality_of :percentage, greater_than_or_equal_to: 1
-  validates_presence_of :threshold, numbericality: true
+  validates_presence_of :threshold, numericality: true
 
 
   belongs_to :merchant
