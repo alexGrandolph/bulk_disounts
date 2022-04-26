13d4d0841b1513a4c18491fd6043ea62f42ebd2f
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-04-26 16:03:56 -0600

    Refactor: refactor holiday poro test to reflect including date

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   resources :merchants, only: [:show] do
     resources :items, except: [:destroy]
     resources :invoices, only: [:index, :show] 
-    # resources :invoice_items, only: [:index, :show, :update]
     resources :dashboard, only: [:index]
+    
   end
 
   resources :admin, controller: 'admin/dashboard', only: [:index]
@@ -15,11 +15,8 @@ Rails.application.routes.draw do
 
     resources :invoices, only: [:index, :show, :update]
     resources :merchants, except: [:destroy]
-
- 
-    # resources :merchants
-
   end
+
   # patch 'admin/merchants/:id', to: 'admin/merchants#update'
   patch '/merchants/:merchant_id/invoice_items', to: 'invoice_items#update'
   
@@ -32,27 +29,4 @@ Rails.application.routes.draw do
   get '/merchants/:merchant_id/bulk_discounts/:id/edit', to: 'bulk_discounts#edit'
   patch '/merchants/:merchant_id/bulk_discounts/:id', to: 'bulk_discounts#update'
    
-  # get '/admin/invoices', to: 'admin/invoices#index'
-  # get '/admin/invoices/:id', to: 'admin/invoices#show'
-
-  # get '/merchants/:id/invoices', to: 'merchant_invoices#index'
-  # get '/merchants/:merchant_id/invoices/:id', to: 'merchant_invoices#show'
-
-
-  # get '/admin', to: "admin/dashboard#index"
-  # get '/admin/merchants', to: 'admin/merchants#index'
-
-  # get '/admin/merchants/new', to: 'admin/merchants#new'
-  # post '/admin/merchants', to: 'admin/merchants#create'
-
-
-  # get '/admin/merchants/:id', to: 'admin/merchants#show'
-  # get '/admin/merchants/:id/edit', to: 'admin/merchants#edit'
-  # patch '/admin/merchants/:id', to: 'admin/merchants#update'
-  # get '/admin/invoices/:id', to: 'admin/invoices#show'
-
-  
-
-  # get '/merchants/:id/invoices', to: 'merchant_invoices#index'
-
 end
