dfd76c5bd893cc4c2ed0180adf0268ffc47aab87
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-04-26 15:48:51 -0600

    Refactor: add date to holiday api use in bulk_discounts index page

--- app/poros/holiday.rb
+++ app/poros/holiday.rb
@@ -2,13 +2,21 @@ class Holiday
   attr_reader :next_holidays
 
   def initialize(data)
-    @next_holidays = []
+    # @next_holidays = []
+    # first_three = data[0..2]
+    # first_three.each do |holiday|
+    #   next_holidays << holiday[:localName]
+      
+    # end
+    # @next_holidays
+
+    @next_holidays = {}
     first_three = data[0..2]
     first_three.each do |holiday|
-      next_holidays << holiday[:localName]
-      
-    end
-    @next_holidays
+      @next_holidays[holiday[:localName]] = holiday[:date]
+    end 
+    # binding.pry
+
   end 
 
 
