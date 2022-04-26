f2343ece864cfb28030ffc0e3e96a7217746b7b9
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-04-26 10:54:00 -0600

    Test: add test for holiday poro

--- app/poros/holiday.rb
+++ app/poros/holiday.rb
@@ -4,13 +4,11 @@ class Holiday
   def initialize(data)
     @next_holidays = []
     first_three = data[0..2]
-
     first_three.each do |holiday|
       next_holidays << holiday[:localName]
       
     end
     @next_holidays
-    # binding.pry
   end 
 
 
